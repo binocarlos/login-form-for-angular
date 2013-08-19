f61a624a47deb2e9006292d5db0079d67c1493db
Kai Davenport <kaiyadavenport@gmail.com>
2013-08-19 14:21:23 +0000

    updated help

--- README.md
+++ README.md
@@ -9,6 +9,18 @@ angular.js directive that renders a login / register form using Twitter bootstra
 
 ## usage
 
+To make the form look nice - you should have a copy of Twitter Boostrap CSS - to make it work, you need angular js on the page.
+
+Then - include your component's build.js on the page:
+
+```html
+<link rel="stylesheet" href="css/bootstrap.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.min.js"></script>
+<script src="build/build.js"></script>
+```
+
+In our custom build for the page:
+
 ```js
 // we must register the component with angular
 require('login-form-for-angular');
@@ -17,16 +29,32 @@ angular
 	.module("myModule", [
 		"loginForm"
 	])
-	.controller('MyCtrl', function(){
+	.controller('MyCtrl', function($location){
 
 		// the user has logged in
-		$scope.$on('loginForm:login', function($e, form_data){
+		$scope.$on('loginForm:login', function($e, form_data, callback){
 
+			// do our login stuff
+			customlogin(form_data, function(error){
+
+				if(error){
+					callback(error);
+				}
+				else{
+					// we will redirect the page - no need to run the callback
+					$location.path('/secure');	
+				}
+				
+				
+			})
+			
 		})
 
 		// the user has registered
-		$scope.$on('loginForm:register', function($e, form_data){
+		$scope.$on('loginForm:register', function($e, form_data, callback){
 
+			// once we have done our register logic - run the callback giving an error as the first argument and the success message as the second
+			callback(null, 'all registered');
 		})
 	})
 ```
--- index.js
+++ index.js
@@ -13,24 +13,71 @@ angular
     //field.required && showvalidate && containerForm[field.name].$invalid
     return {
       restrict:'EA',
-      scope:{
-        loginUrl:'@',
-				registerUrl:'@'
-      },
+      scope:true,
       transclude:true,
       replace:true,
       template:template,
       controller:function($scope){
-        $scope.login = function(){
-
+        $scope.mode = 'login';
+        $scope.setmode = function(mode){
+          $scope.mode = mode;
         }
 
-        $scope.register = function(){
-          
+        $scope.login = {
+          username:'',
+          password:''
         }
+
+        $scope.register = {
+          username:'',
+          password:'',
+          fullname:'',
+          email:''
+        }
+
+        $scope.active = true;
+
+        $scope.showloginvalidate = false;
+
+        $scope.loginSubmit = function(){
+          $scope.active = true;
+          $scope.showloginvalidate = true;
+          if(!$scope.loginForm.$valid){
+            return;
+          }
+
+          $scope.$emit('loginForm:login', $scope.login, function(error, message){
+            if(error){
+              $scope.loginmessage = error;
+            }
+          });
+          return false;
+        }
+
+        $scope.showregistervalidate = false;
+
+        $scope.registerSubmit = function(){
+          $scope.active = true;
+          $scope.showregistervalidate = true;
+          if(!$scope.registerForm.$valid){
+            return;
+          }
+          $scope.$emit('loginForm:register', $scope.register, function(error, message){
+            if(error){
+              $scope.registermessage = error;
+              return;
+            }
+            $scope.registermessage = '';
+            $scope.postregistermessage = message || '';
+            $scope.login.username = $scope.register.username;  
+            $scope.setmode('login');
+          });
+          return false;
+        }
+
       },
       link: function($scope, iElm, iAttrs, controller) {
-				console.log('yay we are making a login form');
+				
 			}
     }
   })
